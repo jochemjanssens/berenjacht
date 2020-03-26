411f9518779244e887bb018eafd2b5b12e80b115
Jochem Janssens <janssens.jochem@gmail.com>
2020-03-26 17:29:05 +0100

    feat: detect bingos

--- src/App.js
+++ src/App.js
@@ -43,35 +43,95 @@ class App extends React.Component {
     super(props);
     this.state = {
       currentBears: getRandomBears(),
-      clickedBears: []
+      clickedBears: [4],
+      activeBingos: [],
+      showPopup: false,
+      showFinalPopup: false,
     };
+  }
+
+  bearsIncludesValues(bears, values) {
+    return values.filter(value => bears.includes(value)).length === values.length;
+  }
+
+  checkBingo(bears) {
+    const ranges = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ];
+
+    const activeBingos = ranges.map(range => this.bearsIncludesValues(bears, range));
+    return activeBingos;
   }
 
   handleBearClick(key) {
     const { clickedBears } = this.state;
+    let newClickedBears = [];
     if (clickedBears.includes(key)) {
-      const newClickedBears = clickedBears.filter(bear => bear !== key);
-      this.setState({
-        ...this.state,
-        clickedBears: newClickedBears,
-      });
-      
+      newClickedBears = clickedBears.filter(bear => bear !== key);
     } else {
-      this.setState({
-        ...this.state,
-        clickedBears: [
-          ...clickedBears,
-          key
-        ]
-      });
+      newClickedBears = [
+        ...clickedBears,
+        key
+      ];
     }
+
+    const activeBingos = this.checkBingo(newClickedBears);
+    const bingos = activeBingos.filter(bingo => bingo);
+    const oldBingos = this.state.activeBingos.filter(bingo => bingo);
+
+    const showPopup = bingos.length !== oldBingos.length;
+
+    this.setState({
+      ...this.state,
+      clickedBears: newClickedBears,
+      activeBingos,
+      showPopup,
+      showFinalPopup: bingos.length === 6
+    });
+  }
+
+  closePopup() {
+    this.setState({
+      ...this.state,
+      showPopup: false,
+    });
+  }
+
+  restart() {
+    this.setState({
+      currentBears: getRandomBears(),
+      clickedBears: [4],
+      activeBingos: [],
+      showPopup: false,
+      showFinalPopup: false,
+    });
   }
 
   render() {
-    const { clickedBears, currentBears } = this.state;
+    const { clickedBears, currentBears, showPopup, showFinalPopup } = this.state;
 
     return (
       <div className="app">
+        { showFinalPopup && <div className="popup" onClick={() => this.restart()}>
+              <div>
+                <p>Je hebt alle beren gevonden, proficiat!</p>
+               <p>Klik om opnieuw te beginnen</p>
+              </div>
+            </div>
+        }
+        {
+          (showPopup && !showFinalPopup) && <div className="popup" onClick={() => this.closePopup()}>
+              <div>
+                <p>Je hebt een nieuwe bingo, proficiat!</p>
+               <p>Klik om verder te spelen</p>
+              </div>
+            </div>
+        }
         <header>
           <h1>Berenjacht</h1>
           <h2>Er zijn heel veel beertjes verstopt. Help jij ons ze te zoeken. Vind alle beertjes die in het rooster staan.</h2>
