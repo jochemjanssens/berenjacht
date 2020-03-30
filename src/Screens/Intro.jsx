a9d5ea07ef348d6d2edb70c2080ea0b1a7b5f15b
Jochem Janssens <janssens.jochem@gmail.com>
2020-03-30 16:24:42 +0200

    feat: show bear on intro

--- src/Screens/Intro.jsx
+++ src/Screens/Intro.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import bearPicture from '../beer.png';
+
 class Intro extends React.Component {
   render() {
     return (
@@ -9,6 +11,8 @@ class Intro extends React.Component {
           <h1>Berenjacht</h1>
           <h2>Er zijn heel veel beertjes verstopt. Help jij ons ze te zoeken. Vind alle beertjes die in het rooster staan.</h2>
           
+          <img src={bearPicture} alt="bingo beer" width="200" height="200" />
+
           <Link to="/main">Start met zoeken</Link>
         </header>
       </div>
