dd517e53edebebe63ae8789da5bdbc31e28a397b
Jochem Janssens <janssens.jochem@gmail.com>
2020-03-31 17:07:18 +0200

    feat: footer links

--- src/Screens/Intro.jsx
+++ src/Screens/Intro.jsx
@@ -6,7 +6,7 @@ import bearPicture from '../beer.png';
 class Intro extends React.Component {
   render() {
     return (
-      <div className="app">
+      <div className="app intro">
         <header>
           <h1>Berenjacht</h1>
           <h2>Er zijn heel veel beertjes verstopt. Help jij Bazel de beer met al haar vriendjes terug te vinden?</h2>
@@ -21,6 +21,14 @@ class Intro extends React.Component {
             }
           }}>GEVORDERDE: Start met zoeken</Link>
         </header>
+        <footer>
+          <p>
+            Gemaakt met ♥ door <a href="https://janssensjochem.be">Jochem Janssens</a>
+          </p>
+          <p>
+            <a href="https://github.com/jochemjanssens/berenjach">Meewerken aan Berenjacht?</a>
+          </p>
+        </footer>
       </div>
     );
   }
