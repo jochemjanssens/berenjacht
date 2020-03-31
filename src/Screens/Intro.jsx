f1f426e49eef8da49772bbbb037163e7ebe6684e
Jochem Janssens <janssens.jochem@gmail.com>
2020-03-31 17:20:38 +0200

    feat: add corona disclamer

--- src/Screens/Intro.jsx
+++ src/Screens/Intro.jsx
@@ -10,6 +10,7 @@ class Intro extends React.Component {
         <header>
           <h1>Berenjacht</h1>
           <h2>Er zijn heel veel beertjes verstopt. Help jij Bazel de beer met al haar vriendjes terug te vinden?</h2>
+          <h2>OPGELET: Houd steeds 1,5m afstand van de mensen rondom je en was na het berenjagen steeds je handen.</h2>
 
           <img src={bearPicture} alt="bingo beer" width="200" height="200" />
 
